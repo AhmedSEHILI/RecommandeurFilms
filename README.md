ac4a544d465686a8a4571367302a12152516d5cf
Daniel THARMARAJAH <daniel.tharmarajah@edu.univ-paris13.fr>
2025-01-10 23:57:44 +0100

    Restriction a 200 ratings

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # RecommandeurFilms 2025
 ## Requete SPARQL Question 5 
+```sparql
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -20,3 +21,4 @@ SELECT ?f1 ?t1 ?d1 ?u1  ?ng ?n1
 	}
 	
 ORDER BY ASC(?f1)
+```
--- parse.py
+++ parse.py
@@ -14,10 +14,10 @@ liste_genres = ["unknown" , "Action" , "Adventure" , "Animation" ,
               "Children's" , "Comedy" , "Crime" , "Documentary" , "Drama" , "Fantasy" ,
               "Film-Noir" , "Horror" , "Musical" , "Mystery" , "Romance" , "Sci-Fi" ,
               "Thriller" ,"War" , "Western" ]
-mois_num = { 'Jan' : '01', 'Feb' : '02', 'Mar' : '03', 'Apr' : '04' , 'May' : '05',
-    'Jun' : '06', 'Jul' : '07','Aug' : '08', 'Sep' : '09' ,'Oct' : '10', 'Nov' :'11', 'Dec' :'12'
+mois_num = { 'Jan' : 1, 'Feb' : 2, 'Mar' : 3, 'Apr' : 4 , 'May' : 5,
+    'Jun' : 6, 'Jul' : 7,'Aug' : 8, 'Sep' : 9 ,'Oct' : 10, 'Nov' : 11, 'Dec' :12
 }       
-liste_ratings = random.choices(ll,k=500)       
+liste_ratings = random.choices(ll,k=200)       
 for line in liste_ratings :
     print('\t<owl:NamedIndividual rdf:about="{nom_onto}#Rating{id}">\
         \n\t\t<rdf:type rdf:resource="{nom_onto}#Rating"/>\
@@ -67,13 +67,20 @@ for line in ll :
         
     if len(line[2]) != 0:
         dateS = line[2].split('-')
-        dateS[1] = mois_num[dateS[1]]
-        
-    print('\t<owl:NamedIndividual rdf:about="{nom_onto}#Film{id_film}">\
+        print('\t<owl:NamedIndividual rdf:about="{nom_onto}#Film{id_film}">\
         \n\t\t<rdf:type rdf:resource="{nom_onto}#Film"/>'.format(nom_onto = url_onto,id_film = line[0])+genres+
-        '\n\t\t<mv:dateSortie>{dateSortie}</mv:dateSortie>\
+        '\n\t\t<mv:anneeSortie rdf:datatype="http://www.w3.org/2001/XMLSchema#integer">{anneeSortie}</mv:anneeSortie>\
+        \n\t\t<mv:jourSortie rdf:datatype="http://www.w3.org/2001/XMLSchema#integer">{jourSortie}</mv:jourSortie>\
+        \n\t\t<mv:moisSortie rdf:datatype="http://www.w3.org/2001/XMLSchema#integer">{moisSortie}</mv:moisSortie>\
         \n\t\t<mv:imdbURL>{url_film}</mv:imdbURL>\
         \n\t\t<mv:title>{titre}</mv:title>\
-    \n\t</owl:NamedIndividual>'.format(dateSortie=['','-'.join(dateS[::-1])][len(line[2]) != 0],titre = line[1].replace('&','&amp;'), url_film = line[4].replace('&','&amp;')))
+        \n\t</owl:NamedIndividual>'.format(titre = line[1].replace('&','&amp;'), url_film = line[4].replace('&','&amp;'),anneeSortie = int(dateS[2]),moisSortie=mois_num[dateS[1]],jourSortie=int(dateS[0])))
+
+    else :    
+        print('\t<owl:NamedIndividual rdf:about="{nom_onto}#Film{id_film}">\
+            \n\t\t<rdf:type rdf:resource="{nom_onto}#Film"/>'.format(nom_onto = url_onto,id_film = line[0])+genres+
+            '\n\t\t<mv:imdbURL>{url_film}</mv:imdbURL>\
+            \n\t\t<mv:title>{titre}</mv:title>\
+            \n\t</owl:NamedIndividual>'.format(titre = line[1].replace('&','&amp;'), url_film = line[4].replace('&','&amp;')))
 
 print('</rdf:RDF>')
